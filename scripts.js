030443c82ef4658e4fb95ad9192d1c38ed3dd57c
caro-Herrera <ana.herrera@edu.sait.ca>
2024-04-05 09:57:46 -0600

    change

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@
  * Fetches list of Marvel characters.
  */
 async function fetchMarvelCharacters() {
-  const publicKey = 'c160808f6cf8f57217704713bca2d3ef'; // Replace with your Marvel API public key
+  const publicKey = 'c160808f6cf8f57217704713bca2d3ef'; 
   const timestamp = Date.now().toString();
-  const privateKey = '388b6be38d60db77c8d41e807ef48bf211311483'; // Replace with your Marvel API private key
+  const privateKey = '388b6be38d60db77c8d41e807ef48bf211311483'; 
   const hash = md5(timestamp + privateKey + publicKey);
   const url = `https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
 
@@ -19,7 +19,7 @@ async function fetchMarvelCharacters() {
 }
 
 /**
- * Create one card from character data.
+ * card from character data.
  */
 function createCharacterCard(character) {
   return `
@@ -33,16 +33,12 @@ function createCharacterCard(character) {
   `;
 }
 
-/**
- * Create multiple cards from array of character data.
- */
+
 function createCharacterCards(characters) {
   return characters.map(createCharacterCard).join('');
 }
 
-/**
- * Render Marvel characters based on search input.
- */
+
 async function renderMarvelCharacters() {
   const searchBar = document.getElementById('searchbar');
   const searchQuery = searchBar.value.toLowerCase();
@@ -57,9 +53,7 @@ async function renderMarvelCharacters() {
   characterResults.innerHTML = createCharacterCards(filteredCharacters);
 }
 
-// Attach event listener to search bar for dynamic search
 const searchbar = document.getElementById("searchbar");
 searchbar.addEventListener("keyup", renderMarvelCharacters);
 
-// Initial rendering of characters
 renderMarvelCharacters();
